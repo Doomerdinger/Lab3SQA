7ac4a61a7c048e0928e686330176b3d1d7164eb5
Alec Tiefenthal <alectief@gmail.com>
2015-03-26 12:19:25 -0400

    Made test 6 work
    I wrote a function that I think would work for all primes, but then
    determined it was shorter to just add another of statement, so I
    commented it out and add the if.

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -25,8 +25,23 @@ public class PrimeFactor
 		{
 			array.add(2);
 			if(num > 3)
+			{
 				array.add(3);
+				if(num > 5)
+					array.add(5);
+			}
 		}
+//		if(num > 2)
+//			array.add(2);
+//		for(int i = 3; i < num; i+=2)
+//		{
+//			boolean isPrime = true;
+//			for(int x = 5; x < i; x+=2)
+//				if(i % x == 0)
+//					isPrime = false;
+//			if(isPrime)
+//				array.add(i);
+//		}
 		return array;
 	}
 }
